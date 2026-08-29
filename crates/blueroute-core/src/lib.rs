215d0b73bb0cfb79e413dedbedef6b507173b31b
Phillip Chin <ekkus93@gmail.com>
2026-08-29 14:16:48 -0700

    feat: export membership persistence domain types

--- crates/blueroute-core/src/lib.rs
+++ crates/blueroute-core/src/lib.rs
@@ -18,7 +18,9 @@ pub use config::{
 pub use error::{CoreError, ErrorKind};
 pub use health::{HealthComponent, HealthLevel, NodeHealth};
 pub use id::{DisplayName, LinkId, NetworkId, NodeId, SegmentId};
-pub use membership::{MembershipState, NetworkMembership};
+pub use membership::{
+    MembershipRegistry, MembershipState, NetworkMembership, PeerMembership,
+};
 pub use route::{IpPrefix, Route, RouteDestination, RouteOwner};
 pub use topology::{LinkHealth, LinkState, PanLink, PanSegment, Reachability, TopologyGraph};
 
