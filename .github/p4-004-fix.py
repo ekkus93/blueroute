d26cbaeafd647842e08f4446a7951158a8ff67a7
Phillip Chin <ekkus93@gmail.com>
2026-08-29 22:55:57 -0700

    chore: fix P4-004 Clippy warning

--- .github/p4-004-fix.py
+++ .github/p4-004-fix.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 path = Path('crates/blueroute-linux/src/bluez.rs')
 text = path.read_text()
+text = text.replace('const AGENT_INTERFACE: &str = "org.bluez.Agent1";\n', '')
 text = text.replace(
     '''    proxy\n        .set_property(TRUSTED_PROPERTY, trusted)\n        .await\n        .map_err(trust_error)\n}\n\nfn trust_error(error: zbus::Error) -> CoreError {''',
     '''    proxy\n        .set_property(TRUSTED_PROPERTY, trusted)\n        .await\n        .map_err(trust_property_error)\n}\n\nfn trust_property_error(error: zbus::fdo::Error) -> CoreError {\n    let kind = match &error {\n        zbus::fdo::Error::UnknownObject(_) => ErrorKind::InvalidState,\n        zbus::fdo::Error::AccessDenied(_) | zbus::fdo::Error::AuthFailed(_) => {\n            ErrorKind::AuthenticationFailed\n        }\n        _ => ErrorKind::AuthenticationFailed,\n    };\n    CoreError::with_diagnostic(\n        kind,\n        "failed to change Bluetooth trust state",\n        error.to_string(),\n    )\n}\n\nfn trust_error(error: zbus::Error) -> CoreError {''',
